1e14ec277251b4b3b11e7cd32a144edbe2f09b36
Vlad Stanescu <vladstanescu94@gmail.com>
2020-11-20 20:31:50 +0200

    Add constructor to User service

--- Tests/test_user_service.py
+++ Tests/test_user_service.py
@@ -7,15 +7,15 @@ from Services.UserService import UserService
 
 class UserServiceUnitTest(unittest.TestCase):
     def setUp(self):
-        self.user_service = UserService()
+        user_args = ["23.2", "12.6", "http://test.com"]
+        self.user_service = UserService(user_args)
 
     def tearDown(self):
         del self.user_service
 
-    def test_get_user_data_from_args_returns_expected_value(self):
-        user_args = ["23.2", "12.6", "http://test.com"]
+    def test_get_user_data_returns_expected_value(self):
         expected_outcome = UserData(Coordinates(23.2, 12.6), "http://test.com")
-        result = self.user_service.get_user_data_from_args(user_args)
+        result = self.user_service.get_user_data()
 
         self.assertEqual(result.coordinates.latitude,
                          expected_outcome.coordinates.latitude)
@@ -24,7 +24,7 @@ class UserServiceUnitTest(unittest.TestCase):
         self.assertEqual(result.csv_url, expected_outcome.csv_url)
 
     def test_get_user_data_from_args_raises_index_error(self):
-        user_args = []
+        self.user_service.args = []
 
         with self.assertRaises(IndexError):
-            self.user_service.get_user_data_from_args(user_args)
+            self.user_service.get_user_data()
